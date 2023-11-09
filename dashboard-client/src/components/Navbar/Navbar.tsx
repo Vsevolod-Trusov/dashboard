1d002909eab1091c8b98c45707d9749c7d96de0e
sev4kz-home <vsevolod_trusov_00@gmial.com>
2023-11-09 16:06:53 +0300

    fix: change some styles

--- dashboard-client/src/components/Navbar/Navbar.tsx
+++ dashboard-client/src/components/Navbar/Navbar.tsx
@@ -1,25 +1,32 @@
 import { FC } from 'react';
 
-import { NAVBAR } from 'common';
+import { NAVBAR, ROUTES } from 'common';
 
 import styles from './styles';
 
 const Navbar: FC = () => (
   <nav className={styles['navbar']}>
     <div className={styles['navbar-logo']}>
-      <a className={styles['navbar-logo__item']} href='/'>
-        {NAVBAR.LOGO}
+      <a className={styles['navbar-logo__item']} href={ROUTES.MAIN}>
+        <i className='bi bi-radar' />
+        <span>{NAVBAR.LOGO}</span>
       </a>
     </div>
     <div className={styles['links']}>
-      <a className={styles['links__item']} href='/'>
+      <a className={styles['links__item']} href={ROUTES.MAIN}>
         {NAVBAR.DASHBOARD}
       </a>
-      <a className={styles['links__item']} href='/departments'>
+      <a className={styles['links__item']} href={ROUTES.DEPARTMENTS}>
         {NAVBAR.DEPARTMENTS}
       </a>
-      <a className={styles['links__item']} href='/staff'>
+      <a className={styles['links__item']} href={ROUTES.STAFF}>
         {NAVBAR.STAFF}
+      </a>
+      <a className={styles['links__item']} href={ROUTES.SIGN_IN}>
+        {NAVBAR.SIGN_IN}
+      </a>
+      <a className={styles['links__item']} href={ROUTES.SIGN_UP}>
+        {NAVBAR.SIGN_UP}
       </a>
     </div>
   </nav>
