3f3dc8fd1c432c9b081f875997d6dfbcdfbb8dc7
sev4kz-home <vsevolod_trusov_00@gmial.com>
2023-11-10 23:16:41 +0300

    fix: remove unnecessary types

--- dashboard-client/src/types/user.ts
+++
@@ -1,9 +0,0 @@
-export type User = {
-    companyName: string;
-    email: string;
-    role: string;
-    isHeader: boolean;
-    id: string;
-    credentialsId: string;
-    departmentId: string | null;
-  }[]| undefined
