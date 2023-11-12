a3ec887919dfbb39038b998ffd55adb26a6d3493
sev4kz-home <vsevolod_trusov_00@gmial.com>
2023-11-13 00:12:20 +0300

    fix: use chart library

--- dashboard-client/src/index.tsx
+++ dashboard-client/src/index.tsx
@@ -2,6 +2,7 @@ import { RouterProvider } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { httpBatchLink, createTRPCProxyClient } from '@trpc/client';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'assets/styles/index.module.scss';
 
@@ -9,6 +10,25 @@ import { BACKEND_URL } from 'common';
 import Router from 'router';
 
 import type { AppRouter } from '../../dashboard-server/src/main';
+
+import {
+  ArcElement,
+  BarElement,
+  CategoryScale,
+  Chart,
+  Legend,
+  LinearScale,
+  Tooltip,
+} from 'chart.js';
+
+Chart.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  ArcElement,
+  Tooltip,
+  Legend,
+);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
