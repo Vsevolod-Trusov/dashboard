bb31ed619760b25bd4cc315982c200be74f03063
sev4kz-home <vsevolod_trusov_00@gmial.com>
2023-11-13 23:58:30 +0300

    feat: add styles for staff page

--- dashboard-client/src/pages/Staff/Staff.tsx
+++ dashboard-client/src/pages/Staff/Staff.tsx
@@ -1,15 +1,45 @@
 import { FC } from 'react';
+import { Table } from 'react-bootstrap';
+
+import { Statistics, styles as dashboardStyles } from 'pages/Dashboard';
+import { DEPARTMENTS_TITLE, InfoPanel } from 'pages/Dashboard/components';
+import styles from './styles';
 
 import { IStaff } from './types';
 
 const Staff: FC<IStaff> = ({ staff }) => (
-  <div>
-    {staff.map(({ email, companyName }) => (
-      <div key={email}>
-        <div>{email}</div>
-        <div>{companyName}</div>
+  <div className={dashboardStyles['template-wrapper']}>
+    <div className={dashboardStyles['template']}>
+      <InfoPanel forStaff />
+      <div className={dashboardStyles['dashboard-wrapper']}>
+        <div className={dashboardStyles['dashboard-title']}>
+          <h2 className={dashboardStyles['dashboard-title__item']}>
+            {DEPARTMENTS_TITLE}
+          </h2>
+        </div>
+        <div className={styles['table-wrapper']}>
+          <Table striped bordered hover className={styles['table']}>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>email</th>
+                <th>company</th>
+              </tr>
+            </thead>
+            <tbody>
+              {staff.map(({ email, companyName }, index) => (
+                <tr key={email}>
+                  <td>{++index}</td>
+                  <td>{email}</td>
+                  <td>{companyName}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </div>
       </div>
-    ))}
+      <Statistics />
+    </div>
   </div>
 );
 export default Staff;
