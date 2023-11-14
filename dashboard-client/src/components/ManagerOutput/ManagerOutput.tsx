5c4ad059aaeb2c86f9d9832fd372be122da7b36e
sev4kz-home <vsevolod_trusov_00@gmial.com>
2023-11-14 20:23:32 +0300

    feat: develop modal window to output manager info

---
+++ dashboard-client/src/components/ManagerOutput/ManagerOutput.tsx
@@ -0,0 +1,49 @@
+import { Button, Modal } from 'react-bootstrap';
+
+import styles from './styles';
+
+const ProfilesOutput = ({ profile, ...props }: any) => {
+  return (
+    <Modal
+      {...props}
+      size='lg'
+      aria-labelledby='contained-modal-title-vcenter'
+      scrollable
+      centered
+    >
+      <Modal.Header className={styles['modal-header']} closeButton>
+        <Modal.Title id='contained-modal-title-vcenter'>
+          {'Manager'}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body className={styles['modal-body']}>
+        {Object.keys(profile ?? {}).map((key) => (
+          <div key={key} className={styles['data-template']}>
+            {typeof profile[key] === 'object' ? (
+              <div className={styles['item-template']}>
+                {Object.keys(profile[key]).map((innerKey) => (
+                  <div
+                    key={innerKey}
+                    className={styles['item-template__element']}
+                  >
+                    <div>{innerKey}</div>
+                    <div>{profile[key][innerKey]}</div>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <div className={styles['data-template__item']}>
+                <div>{key}</div>
+                <div>{profile[key]}</div>
+              </div>
+            )}
+          </div>
+        ))}
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+export default ProfilesOutput;
---
+++ dashboard-client/src/components/ManagerOutput/index.ts
@@ -0,0 +1,1 @@
+export {default as ManagerOutput} from './ManagerOutput'
