10180584f4f568043356d58ae767450f9f41e261
sev4kz-home <vsevolod_trusov_00@gmial.com>
2023-11-14 20:24:23 +0300

    fix: change logic to use only for profiles array

--- dashboard-client/src/components/ProfilesOutput/ProfilesOutput.tsx
+++ dashboard-client/src/components/ProfilesOutput/ProfilesOutput.tsx
@@ -4,7 +4,7 @@ import { Button, Modal } from 'react-bootstrap';
 import { UserProfile } from '../../../../dashboard-server/src/types';
 import styles from './styles';
 
-const ProfilesOutput = ({ data, forManager, ...props }: any) => {
+const ProfilesOutput = ({ data, ...props }: any) => {
   return (
     <Modal
       {...props}
@@ -14,9 +14,7 @@ const ProfilesOutput = ({ data, forManager, ...props }: any) => {
       centered
     >
       <Modal.Header className={styles['modal-header']} closeButton>
-        <Modal.Title id='contained-modal-title-vcenter'>
-          {forManager ? 'Manager' : 'Profiles'}
-        </Modal.Title>
+        <Modal.Title id='contained-modal-title-vcenter'>Profiles</Modal.Title>
       </Modal.Header>
       <Modal.Body className={styles['modal-body']}>
         {(data ?? EMPTY_ARRAY).map((profile: UserProfile, index: any) => (
