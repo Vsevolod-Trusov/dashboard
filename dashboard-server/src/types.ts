8cc2452e827ce52f6b2639db4347d97c0acfce07
sev4kz-home <vsevolod_trusov_00@gmial.com>
2023-11-18 15:24:58 +0300

    fix: change returned types

--- dashboard-server/src/types.ts
+++ dashboard-server/src/types.ts
@@ -1,12 +1,9 @@
-import Prisma, { Company } from '@prisma/client';
+import Prisma, { Department } from '@prisma/client';
 
 export { type Company } from '@prisma/client';
 export type DepartmentAggregate = {
-  departmentId?: string | null;
-  name?: string | null;
-  _count: {
-    departmentId: number;
-  };
+  id?: string | null;
+  count: number;
 };
 
 export type UserProfile = Omit<Partial<Prisma.Profile>, 'createdAt'> & {
@@ -15,17 +12,12 @@ export type UserProfile = Omit<Partial<Prisma.Profile>, 'createdAt'> & {
   company?: Partial<Omit<Prisma.Company, 'createdAt'>>;
 };
 
-type NameWithCompany = {
-  company?: Pick<Company, 'name'>;
-  name?: string;
-};
-
-export type DepartmentWithProfiles = DepartmentAggregate & {
-  profiles: UserProfile[];
-} & { createdAt?: string | undefined | null } & {
-  departmentName?: string | undefined;
-  names?: NameWithCompany;
-};
+export type DepartmentsWithCount = Pick<Department, 'name' | 'id'> &
+  Partial<{ count: number }> &
+  Partial<{ companyName: string }> &
+  Partial<{
+    profiles: UserProfile[];
+  }> & { createdAt?: string | undefined | null };
 
 export type CompanyType = {
   id: string;
