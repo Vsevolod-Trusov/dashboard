f0eaf51a1169972ef70df19fb7a37a19214637c2
sev4kz-home <vsevolod_trusov_00@gmial.com>
2023-11-18 11:10:36 +0300

    fix: change logic of data output

--- dashboard-client/src/components/ManagerOutput/ManagerOutput.tsx
+++ dashboard-client/src/components/ManagerOutput/ManagerOutput.tsx
@@ -1,6 +1,7 @@
 import { Button, Modal } from 'react-bootstrap';
 
 import styles from './styles';
+import { NO_MANAGER } from './constants';
 
 const ProfilesOutput = ({ profile, ...props }: any) => {
   return (
@@ -17,28 +18,34 @@ const ProfilesOutput = ({ profile, ...props }: any) => {
         </Modal.Title>
       </Modal.Header>
       <Modal.Body className={styles['modal-body']}>
-        {Object.keys(profile ?? {}).map((key) => (
-          <div key={key} className={styles['data-template']}>
-            {typeof profile[key] === 'object' ? (
-              <div className={styles['item-template']}>
-                {Object.keys(profile[key]).map((innerKey) => (
-                  <div
-                    key={innerKey}
-                    className={styles['item-template__element']}
-                  >
-                    <div>{innerKey}</div>
-                    <div>{profile[key][innerKey]}</div>
+        {Object.keys(profile ?? {}).length ? (
+          <>
+            {Object.keys(profile ?? {}).map((key) => (
+              <div key={key} className={styles['data-template']}>
+                {typeof profile[key] === 'object' ? (
+                  <div className={styles['item-template']}>
+                    {Object.keys(profile[key]).map((innerKey) => (
+                      <div
+                        key={innerKey}
+                        className={styles['item-template__element']}
+                      >
+                        <div>{innerKey}</div>
+                        <div>{profile[key][innerKey]}</div>
+                      </div>
+                    ))}
                   </div>
-                ))}
+                ) : (
+                  <div className={styles['data-template__item']}>
+                    <div>{key}</div>
+                    <div>{profile[key]}</div>
+                  </div>
+                )}
               </div>
-            ) : (
-              <div className={styles['data-template__item']}>
-                <div>{key}</div>
-                <div>{profile[key]}</div>
-              </div>
-            )}
-          </div>
-        ))}
+            ))}
+          </>
+        ) : (
+          <div>{NO_MANAGER}</div>
+        )}
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
---
+++ dashboard-client/src/components/ManagerOutput/constants.ts
@@ -0,0 +1,1 @@
+export const NO_MANAGER = 'No Manager';
