5e33b67c1a660807dea35cc30399ecafd0cc3361
sev4kz-home <vsevolod_trusov_00@gmial.com>
2023-11-20 05:25:41 +0300

    fix: change logic of how to show data

--- dashboard-client/src/pages/Department/Department.tsx
+++ dashboard-client/src/pages/Department/Department.tsx
@@ -1,6 +1,8 @@
-import { FC, useState } from 'react';
+import { FC, SyntheticEvent, useState } from 'react';
 import { Button } from 'react-bootstrap';
 
+import { NO_DATA, ZERO } from 'common';
+import { DepartmentOutput, DropModal, ProfileOutput } from 'components';
 import {
   DEPARTMENT_LABEL,
   InfoPanel,
@@ -8,16 +10,27 @@ import {
   Statistics,
   styles as dashboardStyles,
 } from 'pages/Dashboard';
-import { ManagerOutput } from 'components';
 
+import { styles as infoPanel } from 'pages/Dashboard/components/InfoPanel';
+import {
+  DepartmentsWithCount,
+  UserProfile,
+} from '../../../../dashboard-server/src/types';
 import { DEPARTMENTS_TITLE, SHOW_OPEN_MODAL_BUTTON_TITLE } from './constants';
-import { UserProfile } from '../../../../dashboard-server/src/types';
-import { IDepartment } from './types';
 import styles from './styles';
+import { IDepartment } from './types';
 
-const Department: FC<IDepartment> = ({ departments }) => {
+const Department: FC<IDepartment> = ({
+  departments,
+  setDepartmentId,
+  handleDelete,
+}) => {
   const [modalShow, setModalShow] = useState(false);
+  const [dropModalShow, setDropModalShow] = useState(false);
   const [profile, setProfiles] = useState<UserProfile>();
+  const [departmentOutput, setDepartmentOutput] = useState(false);
+  const [selectedDepartment, setSelectedDepartment] =
+    useState<DepartmentsWithCount>();
 
   return (
     <div className={dashboardStyles['template-wrapper']}>
@@ -30,37 +43,113 @@ const Department: FC<IDepartment> = ({ departments }) => {
             </h2>
           </div>
           <div className={styles['departments']}>
-            {departments.map(({ name, count, profiles, createdAt }, index) => (
-              <div className={styles['departments-item']} key={index}>
-                <div className={styles['departments-item__department-value']}>
-                  {DEPARTMENT_LABEL} {name}
-                </div>
-                <div className={styles['departments-item__staff']}>
-                  {STAFF_LABEL} {count}
-                </div>
-                <div className={styles['departments-item__staff']}>
-                  {createdAt}
-                </div>
-                <div className={styles['departments-item__open-manager']}>
-                  <Button
-                    variant='primary'
-                    size='sm'
-                    onClick={() => {
-                      const [profile] = new Array(profiles);
-                      setProfiles(profile as unknown as UserProfile);
-                      setModalShow(true);
-                    }}
-                  >
-                    {SHOW_OPEN_MODAL_BUTTON_TITLE}
-                  </Button>
-                </div>
-              </div>
-            ))}
+            {departments.length ? (
+              <>
+                {departments.map(
+                  (
+                    {
+                      id,
+                      name,
+                      count,
+                      profiles,
+                      companyName,
+                      description,
+                      createdAt,
+                    },
+                    index,
+                  ) => (
+                    <div
+                      className={styles['departments-item']}
+                      key={index}
+                      onClick={() => {
+                        setSelectedDepartment({
+                          id,
+                          count,
+                          name,
+                          companyName,
+                          description: description?.substring(0, 20),
+                          createdAt,
+                        });
+                        setDepartmentOutput(true);
+                      }}
+                    >
+                      <div className={styles['departments-item-wrapper']}>
+                        <div
+                          className={
+                            styles['departments-item-wrapper__department-value']
+                          }
+                        >
+                          {DEPARTMENT_LABEL} {name}
+                        </div>
+                        <div
+                          className={styles['departments-item-wrapper__staff']}
+                        >
+                          {STAFF_LABEL} {count ?? ZERO}
+                        </div>
+                        <div
+                          className={styles['departments-item-wrapper__staff']}
+                        >
+                          {createdAt}
+                        </div>
+                        <div
+                          className={
+                            styles['departments-item-wrapper__open-manager']
+                          }
+                        >
+                          <Button
+                            variant='primary'
+                            size='sm'
+                            onClick={(e: SyntheticEvent) => {
+                              if (e.stopPropagation) e.stopPropagation();
+                              const [profile] = new Array(profiles);
+                              setProfiles(profile as unknown as UserProfile);
+                              setModalShow(true);
+                            }}
+                          >
+                            {SHOW_OPEN_MODAL_BUTTON_TITLE}
+                          </Button>
+                        </div>
+                      </div>
+                      <div>
+                        <button
+                          type='button'
+                          aria-label='Close'
+                          className={`btn-close btn-danger ${styles['departments-button-wrapper']}`}
+                          onClick={(e: SyntheticEvent) => {
+                            if (e.stopPropagation) e.stopPropagation();
+                            setDepartmentId(id);
+                            setDropModalShow(true);
+                          }}
+                        ></button>
+                      </div>
+                    </div>
+                  ),
+                )}
+              </>
+            ) : (
+              <p
+                className={`${infoPanel['no-data-item']} ${infoPanel['no-data-item-block']} ${infoPanel['dashboard-mod']}`}
+              >
+                {NO_DATA}
+              </p>
+            )}
           </div>
-          <ManagerOutput
+          <ProfileOutput
+            forManager
             profile={profile}
             show={modalShow}
             onHide={() => setModalShow(false)}
+          />
+          <DepartmentOutput
+            department={selectedDepartment}
+            show={departmentOutput}
+            onHide={() => setDepartmentOutput(false)}
+          />
+          <DropModal
+            dropAim='department'
+            show={dropModalShow}
+            handleDelete={handleDelete}
+            onHide={() => setDropModalShow(false)}
           />
         </div>
         <Statistics />
