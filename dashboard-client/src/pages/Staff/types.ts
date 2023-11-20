e8c7d16400b6f461b31bd8158f448246d854ea87
sev4kz-home <vsevolod_trusov_00@gmial.com>
2023-11-20 05:26:29 +0300

    fix: add constants change logic of component

--- dashboard-client/src/pages/Staff/types.ts
+++ dashboard-client/src/pages/Staff/types.ts
@@ -1,5 +1,23 @@
+import { Dispatch, SetStateAction } from 'react';
+
 import { UserProfile } from '../../../../dashboard-server/src/types';
 
 export interface IStaff {
   staff: UserProfile[];
+  setStaffId: Dispatch<SetStateAction<string>>;
+  setDropModalShow: Dispatch<SetStateAction<boolean>>;
+  modalShow: boolean;
+  handleDelete: () => void;
+  setProfileOutput: Dispatch<SetStateAction<boolean>>;
+  profileOutput: boolean;
+  setSelectedProfile: React.Dispatch<
+    React.SetStateAction<UserProfile | undefined>
+  >;
+  selectedProfile?: UserProfile | undefined;
+  setTimer: React.Dispatch<React.SetStateAction<NodeJS.Timer | undefined>>;
+  timer?: NodeJS.Timer;
+  setSearchName: React.Dispatch<React.SetStateAction<string | undefined>>;
+  searchName?: string;
+  handleSearch: () => void;
+  handleGetAll: () => void;
 }
