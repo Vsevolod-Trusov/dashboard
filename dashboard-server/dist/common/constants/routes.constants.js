60622f8bac73dedc5d29851c0e35a352566d12a1
@sev4kz <90185312+Vsevolod-Trusov@users.noreply.github.com>
2023-11-20 18:21:40 +0300

    Delete dashboard-server/dist directory

--- dashboard-server/dist/common/constants/routes.constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ROUTES = void 0;
-var ROUTES;
-(function (ROUTES) {
-    ROUTES["TRPC"] = "/trpc";
-})(ROUTES || (exports.ROUTES = ROUTES = {}));
